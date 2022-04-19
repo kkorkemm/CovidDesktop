8e0ecbd3e96e981351406c44b6488ef7aee29ccb
korkem <73012254+kkorkemm@users.noreply.github.com>
2022-04-19 12:36:02 +0600

    Добавьте файлы проекта.

---
+++ CovidDesktop/Base/AppData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidDesktop.Base
+{
+    public class AppData
+    {
+        private static CovidSystemDBEntities context;
+        public static CovidSystemDBEntities GetContext()
+        {
+            if (context == null)
+                context = new CovidSystemDBEntities();
+            return context;
+        }
+
+        /// <summary>
+        /// Текущий пользователь системы (пункт вакцинации или админ)
+        /// </summary>
+        public static VaccinationPoint CurrentUser { get; set; }
+    }
+}
